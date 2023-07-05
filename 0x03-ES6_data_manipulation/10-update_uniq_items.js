6e8183f3fab5c1bb8e156c96f3d37f9d647cc3c8
Chidera2468 <chidera2468@gmail.com>
2023-07-05 22:21:52 +0000

    corrections

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,14 @@
-const updateUniqueItems = (items) => {
-  if (!(items instanceof Map)) {
+export default function updateUniqueItems(map) {
+  const list = map;
+
+  if (list instanceof Map) {
+    for (const [key, value] of list) {
+      if (value === 1) {
+        list.set(key, 100);
+      }
+    }
+  } else {
     throw new Error('Cannot process');
   }
-  for (const [k, v] of items.entries()) {
-    if (v === 1) {
-      items.set(k, 100);
-    }
-  }
-  return items;
-};
-
-export default updateUniqueItems;
+  return list;
+}
